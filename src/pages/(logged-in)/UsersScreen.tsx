b976f8b8afd6be3afd3069841fd4de0d2d36000a
Deveno2222 <dias_2003_2010@mail.ru>
2025-04-22 18:40:40 +0500

    user screen fix map

--- src/pages/(logged-in)/UsersScreen.tsx
+++ src/pages/(logged-in)/UsersScreen.tsx
@@ -163,7 +163,7 @@ const UsersScreen = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[18px]">
         {mockUsers.map((user) => (
-          <UserCard data={user} />
+          <UserCard key={user.id} data={user} />
         ))}
       </div>
     </div>
